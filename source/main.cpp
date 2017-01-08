92e47e5cf895908570e7f89f69567840fa130a06
Dyrand <dyrand@inbox.com>
2017-01-07 19:08:13 -0500

    Added subset functionality

--- source/main.cpp
+++ source/main.cpp
@@ -30,20 +30,6 @@
 #include <map>
 #include <exception>
 
-template <typename IterLhs, typename IterRhs>
-auto findUnionOf(IterLhs lhsIter, const IterLhs& lhsEnd,
-                 const IterRhs rhsBegin, const IterRhs& rhsEnd) {
-    std::vector<std::string> result;
-
-    for(; lhsIter != lhsEnd; ++lhsIter) {
-        if(std::find(rhsBegin, rhsEnd, *lhsIter) != rhsEnd) {
-            result.push_back(*lhsIter);
-        }
-    }
-
-    return result;
-}
-
 const std::map<char,uintmax_t> prime_map{
   {'a',2},{'b',3},{'c',5},{'d',7},{'e',11},{'f',13},{'g',17},{'h',19},
   {'i',23},{'j',29},{'k',31},{'l',37},{'m',41},{'n',43},{'o',47},
@@ -62,12 +48,12 @@ int main(int argc, char* argv[]) {
     }
 
     //Print guranteed maximum number of letters for std::size_t
-    std::cout << "Guranteed maximum letters for a word: " << int(log(float(std::numeric_limits<std::size_t>::max()))/log(float(MAX_PRIME))) << std::endl;
+    std::cout << "Guaranteed maximum letters for a word: " << int(log(float(std::numeric_limits<std::size_t>::max()))/log(float(MAX_PRIME))) << std::endl;
 
     //Check for anagram subset flag, -s
     bool subset = false;
     for(int index(0); index < argc && !subset; ++index) {
-      if(strcmp(argv[index],"-s")) {
+      if(strcmp(argv[index],"-s") == 0) {
         subset = true;
       }
     }
@@ -91,13 +77,18 @@ int main(int argc, char* argv[]) {
       std::transform(currentWord.begin(), currentWord.end(), currentWord.begin(), tolower);
       std::size_t product = 1;
       bool too_long = false;
+      bool potential_overflow = false;
       for(auto& c : currentWord)
       {
         product *= prime_map.at(c);
-        if(product > PRE_MAX_VALUE && &c != &currentWord.back()){
-          //std::cout << currentWord << " is too long" << std::endl;
-          too_long = true;
-          break;
+        if((product > PRE_MAX_VALUE && &c != &currentWord.back()) || potential_overflow) {
+          //In the range of a possible overflow
+          potential_overflow = true;
+          if(product/c < PRE_MAX_VALUE) {
+            //std::cout << currentWord << " is too long" << std::endl;
+            too_long = true;
+            break;
+          }
         }
       }
 
@@ -115,79 +106,38 @@ int main(int argc, char* argv[]) {
 
     std::cout << "Searching for anagrams of " << anagram << std::endl;
 
-    unsigned int product = 1;
+    std::size_t product = 1;
+    bool potential_overflow = false;
     for(auto& c : anagram)
     {
       product *= prime_map.at(c);
-      if(product > PRE_MAX_VALUE && &c != &currentWord.back()){
-        std::cout << currentWord << " is too long" << std::endl;
-        std::exit(EXIT_FAILURE);
-        break;
+      if((product > PRE_MAX_VALUE && &c != &currentWord.back()) || potential_overflow) {
+        //In the range of a possible overflow
+        if(product/c < PRE_MAX_VALUE && potential_overflow) {
+          std::cout << anagram << " is too long" << std::endl;
+          std::exit(EXIT_FAILURE);
+        }
+        potential_overflow = true;
       }
     }
 
-    if(words.find(product) != words.end()) {
-      for(auto& str: words.at(product)) {
-        std::cout << "MATCH: " << str << std::endl;
+    if(subset) {
+      bool match = false;
+      for(auto& pair : words) {
+        if(product % pair.first == 0) {
+          match = true;
+          for(auto& str: pair.second) {std::cout << "MATCH: " << str << std::endl;}
+        }
       }
+      if(!match)
+      {std::cout << "No matches found" << std::endl;}
     }
     else {
-      std::cout << "No matches found" << std::endl;
-    }
-
-
-    /*
-    std::string anagram = argv[1];
-    std::transform(anagram.begin(), anagram.end(), anagram.begin(), tolower);
-
-    std::cout << "Searching for anagrams of " << anagram << std::endl;
-
-    std::sort(anagram.begin(), anagram.end());
-
-    std::vector<std::string> anagramPermutations;
-
-    do {
-        anagramPermutations.push_back(anagram);
-    } while(std::next_permutation(anagram.begin(), anagram.end()));
-
-    std::cout << "There are " << anagramPermutations.size() << " possible words" << std::endl;
-
-
-    std::vector<std::future<std::vector<std::string>>> futures;
-
-    const int jump = 20000;
-    auto wordIter = words.cbegin();
-
-    for(;;) {
-        decltype(words)::const_iterator nextEnd;
-
-        if(std::distance(wordIter, words.cend()) <= jump) {
-            nextEnd = words.cend();
-        } else {
-            nextEnd = wordIter + jump;
-        }
-
-        futures.emplace_back(std::async(std::launch::async, [&anagramPermutations, wordIter, nextEnd](){
-            return findUnionOf(wordIter, nextEnd, anagramPermutations.cbegin(), anagramPermutations.cend());
-        }));
-
-        if(nextEnd == words.cend()) {
-            break;
-        }
-
-        wordIter += jump;
-    }
-
-    bool matchFound = false;
-
-    for(auto& future : futures) {
-        for(auto& word : future.get()) {
-            matchFound = true;
-            std::cout << "MATCH: " << word << std::endl;
-        }
-    }
-
-    if(! matchFound) {
+      if(words.find(product) != words.end()) {
+        for(auto& str: words.at(product)) {std::cout << "MATCH: " << str << std::endl;}
+      }
+      else {
         std::cout << "No matches found" << std::endl;
-    }*/
+      }
+    }
 }
